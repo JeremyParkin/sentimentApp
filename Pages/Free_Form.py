80ad45084544c2426bac372ee19b9046ff9b91ec
Jeremy Parkin <djplana@gmail.com>
2023-09-16 14:21:42 -0400

    FreeForm Page added

--- Pages/Free_Form.py
+++ Pages/Free_Form.py
@@ -19,14 +19,14 @@ cost_per_sec = 0.001400
 st.title("MIG Sentiment Tool")
 st.subheader("Experimental")
 
-named_entity = st.text_input("What brand, org, or person should the sentiment be based on?", max_chars=100,
-                             key="named_entity")
-
-if len(named_entity) < 1:
-    named_entity = "the organization"
-
+# named_entity = st.text_input("What brand, org, or person should the sentiment be based on?", max_chars=100,
+#                              key="named_entity")
+#
+# if len(named_entity) < 1:
+#     named_entity = "the organization"
+#
 analysis_placeholder = "Eg. As an academic institution, news stories covering academic presentations are typically positive or neutral, even if the subject of the presentation is not."
-analysis_placeholder2 = f"As a guideline, positive stories might focus on {named_entity}'s good qualities, values, and successes as demonstrated by it and its representatives.  Negative might focus on {named_entity}'s legal or financial difficulties, lawsuits or accusations against it or its representatives, safety issues, racism, abuse, harassment, fraud, or other types of misconduct."
+# analysis_placeholder2 = f"As a guideline, positive stories might focus on {named_entity}'s good qualities, values, and successes as demonstrated by it and its representatives.  Negative might focus on {named_entity}'s legal or financial difficulties, lawsuits or accusations against it or its representatives, safety issues, racism, abuse, harassment, fraud, or other types of misconduct."
 
 analysis_note = st.text_area("Any special note on sentiment approach?", max_chars=600, key="analysis_notes",
                              help="This will be added as a note in the prompt for each story. Use it as is or feel free to, edit, delete, or replace it as needed.",
@@ -34,7 +34,8 @@ analysis_note = st.text_area("Any special note on sentiment approach?", max_char
 
 c1, c2, c3 = st.columns(3)
 with c1:
-    sentiment_switch = st.radio("Sentiment Type", ['3-way', '5-way'], )
+    st.write("")
+    # sentiment_switch = st.radio("Sentiment Type", ['3-way', '5-way'], )
 with c2:
     random_sample = st.radio('Randomize Sample?', ['No', 'Yes'])
 
@@ -42,25 +43,25 @@ with c3:
     action_rows = st.number_input('Limit rows for testing (max 100)', min_value=1, value=5, max_value=600)
 
 
-if sentiment_switch == '5-way':
-    sentiment_type = "VERY POSITIVE / MODERATELY POSITIVE / NEUTRAL / MODERATELY NEGATIVE / VERY NEGATIVE / UNAVAILABLE"
-    sentiment_rubric = f"""
-            \nVERY POSITIVE: A positive portrayal of {named_entity}, focusing on its merits, successes, or positive contributions. A positive headline or first sentence is a good clue of a VERY POSITIVE story.
-            \nSOMEWHAT POSITIVE: A net positive view of {named_entity}, but with some minor reservations or criticisms, maintaining a supportive stance overall.
-            \nNEUTRAL: A passing mention or objective perspective of {named_entity}, balancing both praise and critique without favoring either.
-            \nSOMEWHAT NEGATIVE: A mildly negative depiction of {named_entity}, highlighting its shortcomings, but may acknowledge some positive elements.
-            \nVERY NEGATIVE: A strongly critical portrayal of {named_entity}, emphasizing significant concerns or failings.
-            \nUNAVAILABLE: {named_entity} is not mentioned, or there is not adequate information to choose one of the other options.
-            """
-
-else:
-    sentiment_type = "POSITIVE / NEUTRAL / NEGATIVE / UNAVAILABLE"
-    sentiment_rubric = f"""
-            \nPOSITIVE: A net positive portrayal of {named_entity}, focusing on its merits and successes.
-            \nNEUTRAL: A passing mention or objective perspective of {named_entity}, balancing both praise and critique without favoring either.
-            \nNEGATIVE: A net critical portrayal of {named_entity}, emphasizing significant concerns or failings.
-            \nUNAVAILABLE: {named_entity} is not mentioned, or there is not adequate information to choose one of the other options.
-            """
+# if sentiment_switch == '5-way':
+#     sentiment_type = "VERY POSITIVE / MODERATELY POSITIVE / NEUTRAL / MODERATELY NEGATIVE / VERY NEGATIVE / UNAVAILABLE"
+#     sentiment_rubric = f"""
+#             \nVERY POSITIVE: A positive portrayal of {named_entity}, focusing on its merits, successes, or positive contributions. A positive headline or first sentence is a good clue of a VERY POSITIVE story.
+#             \nSOMEWHAT POSITIVE: A net positive view of {named_entity}, but with some minor reservations or criticisms, maintaining a supportive stance overall.
+#             \nNEUTRAL: A passing mention or objective perspective of {named_entity}, balancing both praise and critique without favoring either.
+#             \nSOMEWHAT NEGATIVE: A mildly negative depiction of {named_entity}, highlighting its shortcomings, but may acknowledge some positive elements.
+#             \nVERY NEGATIVE: A strongly critical portrayal of {named_entity}, emphasizing significant concerns or failings.
+#             \nUNAVAILABLE: {named_entity} is not mentioned, or there is not adequate information to choose one of the other options.
+#             """
+#
+# else:
+#     sentiment_type = "POSITIVE / NEUTRAL / NEGATIVE / UNAVAILABLE"
+#     sentiment_rubric = f"""
+#             \nPOSITIVE: A net positive portrayal of {named_entity}, focusing on its merits and successes.
+#             \nNEUTRAL: A passing mention or objective perspective of {named_entity}, balancing both praise and critique without favoring either.
+#             \nNEGATIVE: A net critical portrayal of {named_entity}, emphasizing significant concerns or failings.
+#             \nUNAVAILABLE: {named_entity} is not mentioned, or there is not adequate information to choose one of the other options.
+#             """
 
 model_call = "replicate/llama-2-70b-chat:58d078176e02c219e11eb4da5a02a7830a283b14cf8f94537af893ccff5ee781"
 
@@ -70,8 +71,6 @@ st.write(f"Estimated cost based on max rows: ${estimated_cost:.2f}")
 
 def prompt_preview():
     prompt_text = f"""
-                You are acting as an entity sentiment AI, indicating how a news story portrays {named_entity} based on these options: 
-                \n{sentiment_rubric}
                 \n{analysis_note}                
                 \nHere is the news story...
             """
@@ -108,13 +107,13 @@ with st.form('User Inputs'):
 
     submitted = st.form_submit_button("Submit")
 
-if submitted and (csv_file is None or named_entity == "the organization"):
+if submitted and (csv_file is None):
     st.error('Missing required form inputs above.')
 
 
 elif submitted:
     replicate_api = st.secrets['REPLICATE_API_TOKEN']
-    df['Entity Sentiment'] = ""
+    df['Freeform Analysis'] = ""
 
     # Progress bar initialization
     progress = st.progress(0)
@@ -122,15 +121,13 @@ elif submitted:
     to_be_done = min(action_rows, number_of_rows)
 
 
-    def generate_prompt(row, named_entity):
+    def generate_prompt(row):
         prompt_text = f"""
-            You are acting as an entity sentiment AI, indicating how a news story portrays {named_entity} based on these options: 
-            \n{sentiment_rubric}
             \n{analysis_note} 
 
             Generate a JSON formatted response with the following fields:
                 {{
-                'sentiment': ' '
+                'label': ' '
                 }}
 
             NOTES:
@@ -154,7 +151,7 @@ elif submitted:
             progress.progress((i + 1) / to_be_done)
             continue
 
-        story_prompt = generate_prompt(row, named_entity)
+        story_prompt = generate_prompt(row)
 
         try:
             start_time = time.time()  # Record the start time
@@ -179,8 +176,8 @@ elif submitted:
 
                 # st.write(json_response)
                 response_json = json.loads(json_response)
-                sentiment = response_json['sentiment']
-                df.at[i, 'Entity Sentiment'] = sentiment
+                label = response_json['label']
+                df.at[i, 'Freeform Analysis'] = label
 
                 break  # If successful, break out of the retry loop
 
@@ -196,7 +193,7 @@ elif submitted:
         # Update progress bar
         progress.progress((i + 1) / to_be_done)
 
-    required_cols = ['Headline', 'Snippet', 'Entity Sentiment']
+    required_cols = ['Headline', 'Snippet', 'Freeform Analysis']
     df_display = df.filter(required_cols)
     st.dataframe(df_display, hide_index=True)
 
@@ -207,8 +204,8 @@ elif submitted:
     writer.close()  # Use writer.close() instead of writer.save()
     output.seek(0)
     st.download_button(
-        label="Download entity analysis as Excel file",
+        label="Download analysis as Excel file",
         data=output,
-        file_name="entity_analysis.xlsx",
+        file_name="freeform_analysis.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
