fbd6ebcd5d3cded3ee10b62450dbbeb3550a915a
Jeremy Parkin <djplana@gmail.com>
2024-12-09 12:56:43 -0500

    requirements update+

--- pages/3-Toning_Interface.py
+++ pages/3-Toning_Interface.py
@@ -364,7 +364,7 @@ else:
                         story_prompt = f"\n{st.session_state.sentiment_instruction}\nThis is the news story:\n{head}\n{body}"
 
                         response = client.chat.completions.create(
-                            model="gpt-4o-mini",
+                            model="gpt-4o",
                             messages=[
                                 {"role": "system", "content": "You are a highly knowledgeable media analysis AI."},
                                 {"role": "user", "content": story_prompt}])
