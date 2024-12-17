174636c8995ff433fb088ec92a0a74925365305a
Jeremy Parkin <djplana@gmail.com>
2024-12-16 23:16:24 -0500

    config fix for single record media types

--- pages/2-Configuration.py
+++ pages/2-Configuration.py
@@ -84,27 +84,6 @@ def preprocess_online_news(df):
 
 
 
-# def cluster_similar_stories(df, similarity_threshold=0.85):
-#     """Cluster similar stories using agglomerative clustering."""
-#     tfidf_vectorizer = TfidfVectorizer()
-#     tfidf_matrix = tfidf_vectorizer.fit_transform(df['Normalized Headline'] + " " + df['Normalized Snippet']).toarray()
-#
-#     # Compute cosine distances
-#     cosine_distance_matrix = cosine_distances(tfidf_matrix)
-#
-#     # Use Agglomerative Clustering with a distance threshold
-#     clustering = AgglomerativeClustering(
-#         n_clusters=None,  # Let the algorithm decide the number of clusters
-#         metric="precomputed",  # Use precomputed cosine distances
-#         linkage="average",  # Average linkage for cosine distances
-#         distance_threshold=1 - similarity_threshold  # Convert similarity to distance
-#     )
-#     cluster_labels = clustering.fit_predict(cosine_distance_matrix)
-#
-#     # Add cluster labels as 'Group ID'
-#     df['Group ID'] = cluster_labels
-#     return df
-
 def cluster_similar_stories(df, similarity_threshold=0.92):
     """Cluster similar stories using agglomerative clustering."""
     tfidf_vectorizer = TfidfVectorizer()
@@ -402,39 +381,7 @@ else:
             if 'Flagged for Review' not in st.session_state.df_traditional.columns:
                 st.session_state.df_traditional['Flagged for Review'] = False
 
-            # st.session_state.df_traditional = st.session_state.df_traditional.dropna(thresh=3)
 
-            # if random_sample == 'Yes, take a sample':
-            #     def calculate_sample_size(N, confidence_level=0.95, margin_of_error=0.05, p=0.5):
-            #         # Z-score for 95% confidence level
-            #         Z = 1.96  # 95% confidence
-            #
-            #         numerator = N * (Z ** 2) * p * (1 - p)
-            #         denominator = (margin_of_error ** 2) * (N - 1) + (Z ** 2) * p * (1 - p)
-            #
-            #         return math.ceil(numerator / denominator)
-            #
-            #
-            #     # Example usage
-            #     population_size = len(st.session_state.full_dataset)
-            #     sample_size = calculate_sample_size(population_size)
-            #
-            #     st.session_state.sample_size = sample_size
-            #
-            #     # Take a random sample of the DataFrame
-            #     if sample_size < population_size:
-            #         df = st.session_state.df_traditional.sample(n=sample_size, random_state=1).reset_index(
-            #             drop=True)  # n=sample_size, random_state=1
-            #     else:
-            #         # If the sample size is greater than or equal to the population, use the entire DataFrame
-            #         df = st.session_state.df_traditional.copy()
-            #
-            #     st.write(f"Full data size: {len(st.session_state.df_traditional)}")
-            #     st.write(f"Calculated sample size: {len(df)}")
-            #
-            #
-            # else:
-            #     df = st.session_state.df_traditional
 
             if st.session_state.sentiment_opinion == True:
                 # Ensure the "Sentiment Opinion" column exists in both dataframes
@@ -444,43 +391,6 @@ else:
                 if 'Sentiment Opinion' not in st.session_state.df_traditional.columns:
                     df['Sentiment Opinion'] = None
 
-            # df['Headline'] = df['Headline'].apply(remove_extra_spaces)
-            # df['Snippet'] = df['Snippet'].apply(remove_extra_spaces)
-            # df['Snippet'] = df['Snippet'].apply(clean_snippet)
-            # df['Normalized Headline'] = df['Headline'].apply(normalize_text)
-            # df['Normalized Snippet'] = df['Snippet'].apply(normalize_text)
-            #
-            # similarity_matrix = calculate_similarity(df)
-            #
-            # duplicate_groups = identify_duplicates(similarity_matrix)
-            #
-            # # Assign group IDs
-            # group_ids = assign_group_ids(duplicate_groups)
-            # df['Group ID'] = df.index.map(group_ids)
-            #
-            # # Drop the normalized columns
-            # df = df.drop(columns=['Normalized Headline', 'Normalized Snippet'])
-            #
-            # st.session_state.df_traditional = df.copy()
-            #
-            # # Calculate group counts
-            # group_counts = df.groupby('Group ID').size().reset_index(name='Group Count')
-            #
-            # # Group by 'Group ID' and keep the 'Group ID' column
-            # unique_stories = df.groupby('Group ID').agg(lambda x: x.iloc[0]).reset_index()
-            #
-            # # Merge group counts with unique_stories
-            # unique_stories_with_counts = unique_stories.merge(group_counts, on='Group ID')
-            #
-            # st.write('Unique Stories with Counts')
-            # st.write(unique_stories_with_counts)  # NO GROUP ID here
-            #
-            # # Sort unique stories by group count in descending order
-            # unique_stories_sorted = unique_stories_with_counts.sort_values(by='Group Count',
-            #                                                                ascending=False).reset_index(drop=True)
-            #
-            # # Update the session state
-            # st.session_state.unique_stories = unique_stories_sorted
             st.rerun()
 
 
@@ -493,10 +403,6 @@ else:
         st.write(f"Unique stories in data: {len(st.session_state.unique_stories)}")
         st.dataframe(st.session_state.unique_stories)
 
-        # st.success('Configuration Completed!')
-        # st.write(f"Full data size: {len(st.session_state.full_dataset)}")
-        # st.write(f"Calculated sample size: {st.session_state.sample_size}")
-        # st.write(f"Unique stories in sample: {len(st.session_state.unique_stories)}")
 
 
         def reset_config():
